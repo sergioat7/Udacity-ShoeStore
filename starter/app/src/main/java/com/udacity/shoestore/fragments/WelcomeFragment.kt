b836644502fb8851d9de289e371072ff7fc5827b
Sergio Aragonés <sergioat7@gmail.com>
2021-05-29 21:55:31 +0200

    Created navigation file and destinations

---
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 29/5/2021
+ */
+
+package com.udacity.shoestore.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    companion object {
+        fun newInstance() = WelcomeFragment()
+    }
+}
