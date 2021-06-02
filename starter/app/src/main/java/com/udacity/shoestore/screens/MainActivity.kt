86ae7c4c8c7f3ce77e8abfdb772c7f35c8671051
Sergio Aragonés <sergioat7@gmail.com>
2021-06-03 00:04:22 +0200

    Set toolbar as action bar and added support for up button

--- starter/app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/MainActivity.kt
@@ -1,7 +1,15 @@
+/*
+ * Copyright (c) 2021 Sergio Aragonés. All rights reserved.
+ * Created by Sergio Aragonés on 29/5/2021
+ */
+
 package com.udacity.shoestore.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -12,5 +20,17 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
