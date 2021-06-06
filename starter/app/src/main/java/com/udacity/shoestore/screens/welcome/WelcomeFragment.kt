6bdf38ee2b29e6e93184694f2bf1e3c7a3d6d4e9
Sergio Aragonés <sergioat7@gmail.com>
2021-06-06 17:44:38 +0200

    Navigate to instructions view

--- starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -6,11 +6,11 @@
 package com.udacity.shoestore.screens.welcome
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
+import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -24,10 +24,24 @@ class WelcomeFragment : Fragment() {
         binding = FragmentWelcomeBinding.inflate(layoutInflater, container, false)
         with(binding) {
             viewPager2Welcome.adapter = WelcomePagerAdapter(this@WelcomeFragment)
-            TabLayoutMediator(tabLayoutWelcome, viewPager2Welcome){ _, _ ->
+            TabLayoutMediator(tabLayoutWelcome, viewPager2Welcome) { _, _ ->
             }.attach()
 
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.skip_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.skip) {
+            findNavController().navigate(R.id.action_welcomeFragment_to_instructionsFragment)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
